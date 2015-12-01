cd03692748d4999561faa32e7cb2db5da9bb5275
batangpinangga <Alena@Alenas-MacBook-Pro.local>
2015-12-01 12:44:08 +0200

    changed filename of term project and implemented Song.java

---
+++ The_Term_Project/src/Song.java
@@ -0,0 +1,47 @@
+public class Song {
+	private String artist;
+	private String title;
+	private String tags;
+	private String mood;
+	
+	public Song(String artist, String title, String tags, String mood){
+		this.setArtist(artist);
+		this.setTitle(title);
+		this.setTags(tags);
+		this.setMood(mood);
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getMood() {
+		return mood;
+	}
+
+	public void setMood(String mood) {
+		this.mood = mood;
+	}
+
+	public String getTags() {
+		return tags;
+	}
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+	
+	
+}
